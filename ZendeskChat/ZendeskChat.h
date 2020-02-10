afcfc47c1569e27fe073e101c1c4fd75b1a7c7d6
Marius <marius@wahanda.com>
2020-02-10 16:49:04 +0200

    Created framework to hold Zendesk frameworks and resources for Carthage

---
+++ ZendeskChat/ZendeskChat.h
@@ -0,0 +1,19 @@
+//
+//  ZendeskChat.h
+//  ZendeskChat
+//
+//  Created by Marius Kazemekaitis on 2020-02-10.
+//  Copyright © 2020 Zendesk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZendeskChat.
+FOUNDATION_EXPORT double ZendeskChatVersionNumber;
+
+//! Project version string for ZendeskChat.
+FOUNDATION_EXPORT const unsigned char ZendeskChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZendeskChat/PublicHeader.h>
+
+
